3d8055dd06786edaf4a1b57161b93ba85f8bc9e7
mimi3403 <mimicp9464@gmail.com>
2022-10-04 14:19:16 -0400

    feat: group added cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,5 @@
 import CartContext from "./cart-context";
 import { useReducer } from "react";
-import { act } from "react-dom/test-utils";
 
 const defaultCartState = {
   items: [],
@@ -8,9 +7,24 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
