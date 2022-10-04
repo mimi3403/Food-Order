6888184aee35c086dc00a3f565119b4eac1e7b53
mimi3403 <mimicp9464@gmail.com>
2022-10-04 13:24:30 -0400

    Feat: add cart items

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,14 @@
 import classes from "./Input.module.css";
+import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input type="text" {...props.input} />
+      <input ref={ref} type="text" {...props.input} />
       {/* {...props.input}  to pass all other configuration data that we might receive from props.input to this input element here as props. This ensures that all the key value pairs in this input object which we receive on props.input are added as props to input*/}
     </div>
   );
-};
+});
 
 export default Input;
